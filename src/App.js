466aa444aac443175e2583903618b64d1c0f0c6e
Ayush <ayush.ramkuar@gmail.com>
2017-12-28 23:04:58 -0500

    Basic Server Functionality and Authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import blankalbum from './blankalbumart.png';
+import { Paper } from 'material-ui';
 import './App.css';
+import socketIo from 'socket.io-client';
+
+const socket = socketIo.connect();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlistUrl: '',
+      playlistAlbumArt: blankalbum,
+    };
+  }
+
   render() {
     return (
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Paper className="header">
+          <header>
+            <h1 className="App-title">Spotify Calc</h1>
+            <h2>Filesizes and other various information about a playlist</h2>            
+          </header>
+        </Paper>
+        <Paper className="playlist">
+          <img src={this.state.playlistAlbumArt} className="App-logo" alt="Album Art" />
+        </Paper>
+        <Paper className="playlistInfo">
+
+        </Paper>
+        
+      </MuiThemeProvider>
+      /**
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={blankalbum} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      */
     );
   }
 }
