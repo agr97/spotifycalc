739459dbc766048368a1de06d1cf5918681e98b4
Ayush <ayush.ramkuar@gmail.com>
2018-01-02 23:44:50 -0500

    v1.0

--- server.js
+++ server.js
@@ -3,55 +3,77 @@ const http = require('http');
 const path = require('path');
 const socketIo = require('socket.io');
 const authentication = require('./authentication');
-const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const server = app.listen(8080);// http.createServer(app);
+const io = socketIo.listen(server);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-authentication.spotifyRefreshToken();
+const spotifyApi = authentication.SpotifyWebApi;
+authentication.spotifyRefreshToken(spotifyApi);
 setInterval(() => {
-  authentication.spotifyRefreshToken();
+  authentication.spotifyRefreshToken(spotifyApi);
 }, 3590000);
+
+
+// spotifyApi.getPlaylist('121410021', '4FO2WXjS922s0RheaTYPZK')
+// spotifyApi.getPlaylist('nonnoobgod', '2eIlWTq7gSFGZJXnu0I5DP')
 
 
 io.sockets.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('updateMessages', (data, callback) => {
-    callback(serverMessages);
-  });
+  socket.on('recieveClientPlaylist', (data, callback) => {
+    console.log(`${data.username} ${data.id}`);
 
-  socket.on('receiveClientMessage', (message) => {
-    serverMessages.push(message);
-    console.log(`message recieved: ${message}`);
+    spotifyApi.getPlaylist(data.username, data.id)
+      .then((playlist) => {
+        const songs = playlist.body.tracks.total;
+        console.log(songs);
+        let totallength = 0;
+        const totalrequests = parseInt(songs / 100) + 1;
+        console.log(totalrequests);
+
+        const currentStart = 0;
+
+        function temp() {
+          spotifyApi.getPlaylistTracks(data.username, data.id, { offset: currentStart, limit: 100, fields: 'items(track.duration_ms)' })
+            .then((tracks) => {
+              tracks.body.items.forEach((track) => {
+                totallength += track.track.duration_ms;
+              });
+            }, (err) => {
+              console.log('Something went wrong!', err);
+            });
+        }
+
+        for (let i = 0; i < totalrequests; i++) {
+          temp();
+        }
+
+        let sendTime = 250;
+        if (songs >= 1000) {
+          sendTime = 3000;
+        }
+
+        const t = setInterval(() => {
+          console.log(totallength);
+          callback('1', playlist.body, totallength);
+          clearInterval(t);
+        }, sendTime);
+
+        // length().then(callback('1', playlist.body, totallength));
+      }, (err) => {
+        callback('err', null);
+        console.log(err);
+      });
   });
 });
 
-/** 
-spotifyApi.getPlaylist('nonnoobgod', '7sHlmgCE362bjKmijLEU48')
-  .then(function(data) {
-    console.log('gets this far');
-    console.log('Some information about this playlist', data.body);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
-
-*/
 
 /**
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/build/index.html`));
-});
-
-
-
-
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('listening on *:3000');
 });
 */
--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import blankalbum from './blankalbumart.png';
-import { Paper } from 'material-ui';
+import { Paper, TextField, RaisedButton } from 'material-ui';
 import './App.css';
 import socketIo from 'socket.io-client';
+import url from 'url';
 
 const socket = socketIo.connect();
 
@@ -13,39 +14,138 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loadedPlaylist: '',
       playlistUrl: '',
       playlistAlbumArt: blankalbum,
+      playlistName: '',
+      playlistUsername: '',
+      playlistDisplayUsername: '',
+      textbarUsername: '',
+      playlistID: '',
+      textbarID: '',
+      playlistFollowers: '',
+      errorText: '',
+      validUrl: false,
+      playlistLoaded: false,
+      filesizeNormal: '',
+      filesizeHigh: '',
+      filesizeExtreme: ''
     };
+
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.submitUrl = this.submitUrl.bind(this);
+    this.updatePlaylistInfo = this.updatePlaylistInfo.bind(this);
   }
+
+  componentDidMount() {
+
+  }
+
+  updatePlaylistInfo(playlist, playlistlength) {
+    if (playlist.owner.display_name === null) {
+      this.setState({ playlistDisplayUsername: playlist.owner.id });
+    } else {
+      this.setState({ playlistDisplayUsername: playlist.owner.display_name });
+    }
+
+    const totalseconds = playlistlength / 1000;
+    let megabyesNormal = (totalseconds * 12 / 1024).toFixed(2);
+    let megabyesHigh = (totalseconds * 20 / 1024).toFixed(2);
+    let megabyesExtreme = ((totalseconds * 40) / 1024).toFixed(2);
+
+    this.setState({
+      playlistAlbumArt: playlist.images[0].url,
+      playlistName: playlist.name,
+      playlistID: playlist.id,
+      playlistUsername: playlist.owner.id,
+      playlistFollowers: playlist.followers.total + ' Followers',
+      filesizeNormal: 'File Size at Normal Quality: ' + megabyesNormal  + ' Megabytes',
+      filesizeHigh: 'File Size at High Quality: ' + megabyesHigh + ' Megabytes',
+      filesizeExtreme: 'File Size at Extreme Quality: ' + megabyesExtreme  + ' Megabytes',
+    });
+    
+  }
+
+  submitUrl(event) {
+    event.preventDefault();
+
+    if (this.state.playlistLoaded
+      && this.state.textbarID === this.state.playlistID
+      && this.state.textbarUsername === this.state.playlistUsername) {
+      this.setState({ errorText: 'Already Loaded This Playlist', validUrl: false });
+    } else {
+      socket.emit(
+        'recieveClientPlaylist',
+        { username: this.state.textbarUsername, id: this.state.textbarID },
+        (error, playlist, playlistlength) => {
+          if (error !== 'err') {
+            this.setState({ loadedPlaylist: playlist, playlistLoaded: true });
+            this.updatePlaylistInfo(playlist, playlistlength);
+          } else {
+            this.setState({ errorText: 'Playlist Not Found on Spotify Servers', validUrl: false });
+          }
+        },
+      );
+    }
+  }
+
+  handleUrlChange(event) {
+    this.setState({ playlistUrl: event.target.value });
+    const urlRegexp = /(\/user\/)+(\w+)+(\/playlist\/)+(\w+)/g;
+
+    const parsedUrl = url.parse(event.target.value);
+    const { pathname, hostname } = parsedUrl;
+
+    console.log(url.parse(event.target.value));
+    if (hostname === 'open.spotify.com' && pathname.match(urlRegexp)) {
+      const playlistData = urlRegexp.exec(pathname);
+      this.setState({ errorText: '', validUrl: true });
+      this.setState({ textbarUsername: playlistData[2], textbarID: playlistData[4] });
+      console.log(`${playlistData[2]} , ${playlistData[4]}`);
+    } else {
+      this.setState({ errorText: 'Invalid Playlist Url', validUrl: false });
+    }
+  }
+
+  // https://open.spotify.com/user/nonnoobgod/playlist/2eIlWTq7gSFGZJXnu0I5DP
+  // (\/user\/)+(\w+)+(\/playlist\/)+(\w+)
 
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <Paper className="header">
-          <header>
-            <h1 className="App-title">Spotify Calc</h1>
-            <h2>Filesizes and other various information about a playlist</h2>            
-          </header>
+          <h1 className="App-title">Spotify Calc</h1>
+          <div>Filesize information about a Spotify playlist</div>
+          <form onSubmit={this.submitUrl}>
+            <TextField
+              errorText={this.state.errorText}
+              fullWidth
+              floatingLabelText="Enter A Spotify Playlist URL"
+              value={this.state.playlistUrl}
+              onChange={this.handleUrlChange}
+            />
+            <RaisedButton
+              disabled={!this.state.validUrl}
+              type="submit"
+              label="Submit"
+            />
+          </form>
         </Paper>
         <Paper className="playlist">
           <img src={this.state.playlistAlbumArt} className="App-logo" alt="Album Art" />
+          <div className="playlistMain">
+            <div >{this.state.playlistName}</div>
+            <div >{this.state.playlistDisplayUsername}</div>
+            <div >{this.state.playlistFollowers}</div>
+          </div>
         </Paper>
         <Paper className="playlistInfo">
-
+          <h4>{this.state.filesizeNormal}</h4>
+          <h4>{this.state.filesizeHigh}</h4>
+          <h4>{this.state.filesizeExtreme}</h4>
         </Paper>
-        
+
       </MuiThemeProvider>
-      /**
-      <div className="App">
-        <header className="App-header">
-          <img src={blankalbum} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-      */
     );
   }
 }
